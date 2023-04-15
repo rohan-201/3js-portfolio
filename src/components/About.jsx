7f083946bd74de679e250a5e30ff5c3dc2a7b712
rohan-201 <rohanpoddar780@gmail.com>
2023-04-15 15:41:45 +0530

    added hoc

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,69 @@
 import React from 'react'
-import Tilt from "react-tilt";
+import { Tilt } from 'react-tilt'
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
+import {fadeIn, textVariant} from '../utils/motion'
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
 
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}
+        >Introduction</p>
+        <h2 className={styles.sectionHeadText}
+        >Overview.</h2>
+      </motion.div>
+
+      <motion.p 
+      variants={fadeIn("", "", 0.1, 1)}
+      className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] '
+      >
+        I'am 15 year old and love coding. I am fluent with javascript, typescript, rust, move, python and more. I also know few librarys such as react js, next js and more. I am intreast in web3 and blockchain tecnology. I mosly work on solana. i am also learing more aboutnew blockchains like aptos and sui. With my team i have started a new project called "Maker3" and i am Co-founder of it.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map ((service, index) => (
+          <ServiceCard key={service.title} index=
+          {index} {...service} />
+        ))}
+      </div>
+    </>
   )
 }
 
-export default About
+export default SectionWrapper
+(About, "about") 
 
