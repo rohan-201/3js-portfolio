fd60e3d3877d0871c4c7b6e7ff419f88a760080a
rohan-201 <rohanpoddar780@gmail.com>
2023-04-15 09:38:42 +0530

    increase container width

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,11 +6,21 @@ import { ComputersCanvas } from './canvas';
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx auto'>
-      <div className={'${styles.paddingX} absolute insert-0 top-[120px] max-7xl mx-auto flex flex-row items-start gap-5'}>
+      <div className={'absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5'}>
         <div className='flex flex-col justify-center items-center mt-5'>
-            <div className='w-5 h-5 rounded-full bg-[]'/>
+           <div className='w-5 h-5 rounded-full bg-[hsl(284,100%,41%)]' />
+           <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[hsl(284,100%,41%)]'> Drac</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am a Full-stack web3 developer<br className='sm:block hidden' />
+            I mostly work on solana.
+          </p>
         </div>
       </div>
+      <ComputersCanvas/>
     </section>
   )
 }
